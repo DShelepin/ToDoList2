683e66a9366f519a25e92c6c5f5da3ece14587cb
DShelepin <daniilselepin73@gmail.com>
2023-11-24 02:14:56 +0300

    render tasks added

--- main.js
+++ main.js
@@ -1,12 +1,34 @@
+import { getTasks } from './api/task';
 import { getUser } from './api/user';
+import { renderTasks, renderTasksLoader, renderUser } from './utils/renders';
 import { openTaskModal } from './utils/taskModalHandlers';
+
+function removeUserLoader () {
+   const loader = document.querySelector('#loader').remove()
+
+   if(loader) {
+    loader.remove()
+   }
+}
 
 async function start() {
   try {
     const user = await getUser();
+
     if (!user) {
       window.location.href = '/auth/login.html';
     }
+
+    renderUser(user)
+
+    removeUserLoader()
+
+     renderTasksLoader()
+
+    const tasks = await getTasks()
+
+
+    renderTasks(tasks)
 
     const addTaskButton = document.querySelector('#addTaskButton');
 
